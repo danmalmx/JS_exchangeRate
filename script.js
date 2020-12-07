f81b4d16be8b7d57923e0f0cae41f8350c96904e
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2020-12-07 21:40:24 +0100

    Add exchange rate functionality. Use exchangerate-api

---
+++ script.js
@@ -0,0 +1,38 @@
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+function calculate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+
+    fetch(`https://v6.exchangerate-api.com/v6/a98fd9e1a20fb119ac3b940e/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.conversion_rates[currency_two]
+            
+            rateEl.innerHTML = `1 ${currency_one} = ${rate} ${currency_two}`
+            
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        })
+            
+}
+
+function swapCurrency() {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+
+    calculate();
+}
+
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+swap.addEventListener('click', swapCurrency);
+
+calculate();
